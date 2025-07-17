79556ce339ae9f7163707ed9583a430a1321aa5e
Kádymo <santanakadymo@gmail.com>
2025-07-17 11:59:38 -0300

    FEAT: Using message type from amqp lib

--- email/src/main/java/com/backend/email/consumer/EmailConsumer.java
+++ email/src/main/java/com/backend/email/consumer/EmailConsumer.java
@@ -1,5 +1,6 @@
 package com.backend.email.consumer;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailConsumer {
     @RabbitListener(queues = "email-queue")
-    public void receiveEmail(@Payload String email) {
+    public void receiveEmail(@Payload Message email) {
         System.out.println(email);
     }
 }
