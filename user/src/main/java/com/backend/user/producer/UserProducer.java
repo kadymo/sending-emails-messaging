639afc14977b1755ed5fbde513f9386c0a75ab2f
Kádymo <santanakadymo@gmail.com>
2025-07-18 23:48:06 -0300

    FIX: Injecting dependencies manually

--- user/src/main/java/com/backend/user/producer/UserProducer.java
+++ user/src/main/java/com/backend/user/producer/UserProducer.java
@@ -3,21 +3,19 @@ package com.backend.user.producer;
 import com.backend.user.domain.dto.UserDto;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserProducer {
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate rabbitTemplate;
+    private final Queue queue;
 
-    private final String queueName = "email-queue";
-
-    @Bean
-    public Queue queue() { return new Queue(queueName, true); }
+    public UserProducer(RabbitTemplate rabbitTemplate, Queue queue) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.queue = queue;
+    }
 
     public void sendMessage(UserDto message) {
-        rabbitTemplate.convertAndSend(queueName, message);
+        rabbitTemplate.convertAndSend(queue.getName(), message);
     }
 }
